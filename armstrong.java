f8803a57839093ff9f312ba5fc8f9858c5ce32be
nandan kunchakuri <97663031+nandanspykar@users.noreply.github.com>
2022-01-25 16:00:57 +0530

    Add files via upload

---
+++ armstrong.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class armstrong { public static void main(String[] args) {
+Scanner sc=new Scanner(System.in);
+int originalNumber, remainder, result = 0;
+System.out.println("enter a number");
+int number=sc.nextInt(); originalNumber = number; while (originalNumber != 0)
+{
+remainder = originalNumber % 10;
+result += Math.pow(remainder, 3);
+originalNumber /= 10;
+} if(result == number)
+System.out.println(number + " is an Armstrong number.");
+else
+System.out.println(number + " is not an Armstrong number.");
+}
+}
+
